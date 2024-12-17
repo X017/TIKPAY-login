76ef961252b64da1df739b100ce99322340d6a0d
Parsa <deadlockfree504@gmail.com>
2024-12-17 20:30:33 +0330

    fixed api routes

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from api.v1 import views
 
 urlpatterns = [
-        path('sign-up/',views.SignUpAPI.as_view()),
-        path('upload/<int:pk>/',views.UploadSingUPAPI.as_view()),
+        path('v1/sign-up/',views.SignUpAPI.as_view()),
+        path('v1/upload/<int:pk>/',views.UploadSingUPAPI.as_view()),
 ]
 
 
